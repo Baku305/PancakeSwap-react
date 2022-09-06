2649ff7ab62319988b56b2bc43d461fd7b2adf45
Jonathan Biasella <jonathanbiasella@gmail.com>
2022-09-06 18:34:42 +0200

    StyledButton Component

--- pancakeswap/src/App.js
+++ pancakeswap/src/App.js
@@ -1,11 +1,14 @@
 import './App.scss';
 import { Route, Routes } from 'react-router-dom';
 import { Component } from './omponents';
+import { StyledButton } from './components/Button';
 
 function App() {
   return (
     <Routes>
-      <Route path="" element = {<Component/>}/>
+      <Route path="" element = {<StyledButton primary>Ciao</StyledButton>}/>
+
+
     </Routes>
     
   );
---
+++ pancakeswap/src/components/Color.jsx
@@ -0,0 +1,47 @@
+/* COLORS */
+
+export const color = {
+     primary : '#666171',
+     darkBlack : '#08060b',
+     blackBrand : '#191326',
+     lightBlack : '#222222',
+     borderBlack : '#262130',
+     darkBlackTransp :' #08060b99',
+     ligthTransp:' #fff9',
+    
+     darkGray3 : '#383241',
+     darkGray2 : '#3c3742',
+     darkGray : '#27262c',
+     gray : '#999999',
+     lightGray : '#cccccc',
+     borderGray : '#565065',
+    
+     violetBrand : '#7645d9',
+     violetBrand1: '#280d5f',
+     violetBrand2 :' #9a6aff',
+     violetBrand3 :' #5121b1',
+     lightViolet : '#f4eeff',
+     lightViolet2 :' #b8add2',
+     subTitleViolet : '#7a6eaa',
+     violetSeparator : '#d8cbed',
+    
+     blueBrand : '#1155cc',
+     darkBlue :' #280d5f',
+     darkBlue2 :' #3c1786',
+     tabBlue : '#372f47',
+     hoverMenu: '#353547',
+    
+     pinkBrand : '#ed4b9e',
+     ocra :' #ffb237',
+     ocra2:' #ffcd51',
+     ocra3: '#ffe76a',
+     ocra4: '#ffd800',
+     ocra5: '#fdab32', 
+     whiteBrand :' #ffffff',
+     tiffanyBrand : '#1fc7d4',
+     greenBrand : '#29910d',
+     greyMouse: '#E9F2F6',
+     trasparency :' #00000000', 
+     hoverEffect : '#08060b00',
+}
+
