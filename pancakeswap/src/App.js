17b41996eb3efb19bc053a795c15473ca201765f
Jonathan Biasella <jonathanbiasella@gmail.com>
2022-09-08 18:15:06 +0200

    Create Button component

--- pancakeswap/src/App.js
+++ pancakeswap/src/App.js
@@ -1,17 +1,27 @@
 import './App.scss';
-import { Route, Routes } from 'react-router-dom';
-import { Component } from './omponents';
-import { StyledButton } from './components/Button';
-
+import { Route, Routes, Link, useNavigate } from 'react-router-dom';
+import { Button } from './components/Button';
+import { color } from './components/Color'
 function App() {
+  const Navigate = useNavigate(); 
+
+  
+
   return (
-    <Routes>
-      <Route path="" element = {<StyledButton primary>Ciao</StyledButton>}/>
-
-
-    </Routes>
-    
+    <>
+    <h1  className = "tiffanyBrand">The moon is made of pancakes.</h1>
+    <h1  color = "pinkBrand">The moon is made of pancakes.</h1>
+    <h1  className = "violetBrand">The moon is made of pancakes.</h1>
+      <Routes>
+        <Route path='/' element={<Button color="violetBrand" onClick={()=>Navigate("/2")} buttonStyle={"btn--default"}>ciao</Button>} />
+        <Route path = "/1" element = {<Button buttonStyle={"btn--default"}/>}/>
+        <Route path="/2" element= {<Button  buttonSize={"btn--full"} type="button" as={Link} to="/">Esempio</Button>}/>
+      </Routes>
+    </>
   );
 }
 
 export default App;
+
+
+
--- pancakeswap/src/components/Button.jsx
+++ pancakeswap/src/components/Button.jsx
@@ -1,19 +1,34 @@
-import styled, { DefaultTheme, css } from "styled-components";
-import "../style.scss"
-import { color } from "./Color";
+import "./button.scss";
 
+const STYLES = [
+    "btn--default",
+    "btn--alternative",
+    "btn--info",
+    "btn--connect"
+]; 
 
-export const StyledButton = styled.div
-`@include flexCenter;
-height: 4.8rem;
-width: 16.5rem;
-box-shadow: rgba(14, 14, 44, 0.4) 0px -1px 0px 0px inset;
-border-radius: 1.6rem;
-line-height: 1.6rem;
-font-size: 1.6rem;
-text-align: center;
-letter-spacing: 0.48px;
-padding-inline: 2frem;
-cursor: pointer;
-background-color:${props=> props.primary?color.tiffanyBrand:color.ocra};`
+const SIZES = [
+    "btn--full"
+]
 
+export const Button = ({
+    children, 
+    buttonStyle,
+    buttonSize,
+    onClick, 
+    type,
+    color,
+}) => {
+
+    const checkButtonStyle = STYLES.includes(buttonStyle) 
+    ? buttonStyle 
+    : STYLES[0]; 
+
+    const checkButtonSize = SIZES.includes(buttonSize) && buttonSize
+
+    return(
+    <button className={`${checkButtonStyle} ${checkButtonSize} ${color}`} onClick={onClick} type={type}>
+    {children}
+    </button>
+    )
+}
