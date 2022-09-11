d853f5900f9daf26a38d6e03c909f546ea9a934d
Baku305 <101564468+Baku305@users.noreply.github.com>
2022-09-11 22:59:41 +0200

    feat/navbar init

--- pancakeswap/src/App.js
+++ pancakeswap/src/App.js
@@ -2,16 +2,18 @@ import './App.scss';
 import { Route, Routes, Link, useNavigate } from 'react-router-dom';
 import { Button } from './components/Button';
 import { color } from './components/Color'
+import { Navbar } from './components/navbar/navbar';
 function App() {
   const Navigate = useNavigate(); 
 
-  
+  const menu = [{name:"trade"},{name:"earn"},{name:"win"},{name : "nft"},{name:"···"}]
 
   return (
     <>
     <h1  className = "tiffanyBrand">The moon is made of pancakes.</h1>
     <h1  color = "pinkBrand">The moon is made of pancakes.</h1>
     <h1  className = "violetBrand">The moon is made of pancakes.</h1>
+    <Navbar menu = {menu}/>
       <Routes>
         <Route path='/' element={<Button color="violetBrand" onClick={()=>Navigate("/2")} buttonStyle={"btn--default"}>ciao</Button>} />
         <Route path = "/1" element = {<Button buttonStyle={"btn--default"}/>}/>
