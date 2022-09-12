e98e3382442436ded0cb57d339386c590ecc4b87
Baku305 <101564468+Baku305@users.noreply.github.com>
2022-09-12 17:53:30 +0200

    fix counter style

--- pancakeswap/src/components/banner/banner.jsx
+++ pancakeswap/src/components/banner/banner.jsx
@@ -3,6 +3,8 @@ import { LotteryCounter } from "./lotteryCounter";
 import { useState } from "react";
 import "./_banner.scss";
 import imgLottery from "../../assets/asset 2.png";
+import imgLotteryMobile from "../../assets/lotteryMobile.png"
+import arrowButton from "../../assets/asset 56.svg"
 import { Button } from "../shared/button/Button";
 
 
@@ -15,13 +17,16 @@ export function Banner(props) {
     <>
       <div className={props.className}>
         <div className="lottery-banner">
-          <h2 className="lottery-title">win ${Math.round(lottery)} in lottery</h2>
+          <h2 className="lottery-title">Win ${Math.round(lottery)} in lottery</h2>
           <LotteryCounter 
           ></LotteryCounter>
-          <Button />
+          <div className="button_wrapper">
+          <Button buttonSize = "btn--full" children={<div className="button_label">Play Now <img src = {arrowButton} alt= "->"></img></div>}/>
+          </div>
         </div>
         <div className="lottery-image-container">
           <img src={imgLottery} alt="" className="lottery-image-container-image" />
+          <img src={imgLotteryMobile} alt="" className="lottery-image-container-image-mobile" />
         </div>
       </div>
     </>
--- pancakeswap/src/components/banner/lotteryCounter.jsx
+++ pancakeswap/src/components/banner/lotteryCounter.jsx
@@ -4,20 +4,29 @@ import "./_lotteryCounter.scss";
 import { useCountdown } from "../../custom hooks/useCountDownMaker";
 
 export function LotteryCounter() {
-  const {days, hours, minutes, seconds} = useCountdown("30 sep 2022");
+  const { days, hours, minutes, seconds } = useCountdown("30 sep 2022");
 
-  const [amount,setAmount] = useState({})
+  const [amount, setAmount] = useState({});
 
-  useEffect(()=>{
-    return ()=>setAmount({days, hours, minutes, seconds})
-  },[days, hours, minutes, seconds])
+  useEffect(() => {
+    return () => setAmount({ days, hours, minutes, seconds });
+  }, [days, hours, minutes, seconds]);
 
   return (
     <>
       <div className="lottery-counter" style={{ display: "flex", gap: "10px" }}>
-        <h2>{hours}h</h2>
-        <h2>{minutes}m</h2>
-        <h2>{seconds}s</h2>
+        <div className="lottery-content-wrapper">
+          <h2>{hours}</h2>
+          <h3>h</h3>
+        </div>
+        <div className="lottery-content-wrapper">
+          <h2>{minutes}</h2>
+          <h3>m</h3>
+        </div>
+        <div className="lottery-content-wrapper">
+          <h2>{seconds}</h2>
+          <h3>s</h3>
+        </div>
       </div>
     </>
   );
