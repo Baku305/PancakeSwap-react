363d01858b14d1d62b28944c41b08dbc8e830a16
Martib01 <martybb01@gmail.com>
2022-09-18 13:04:53 +0200

    initial commit

--- pancakeswap/src/App.js
+++ pancakeswap/src/App.js
@@ -1,33 +1,58 @@
-import './App.scss';
-import { Route, Routes, Link, useNavigate } from 'react-router-dom';
-import { Button } from './components/shared/button/Button';
-import { color } from './components/Color'
-import { Navbar } from './components/navbar/navbar';
-import { Tabs } from './components/banner/tabs';
+import "./App.scss";
+import { Route, Routes, Link, useNavigate } from "react-router-dom";
+import { Button } from "./components/shared/button/Button";
+import { color } from "./components/Color";
+import { Navbar } from "./components/navbar/navbar";
+import { Tabs } from "./components/banner/tabs";
+import { Paragraph } from "./components/shared/paragraph/Paragraph";
 function App() {
-  const Navigate = useNavigate(); 
+  const Navigate = useNavigate();
 
-  const menu = [{name:"trade"},{name:"earn"},{name:"win"},{name : "nft"},{name:"···"}]
+  const menu = [
+    { name: "trade" },
+    { name: "earn" },
+    { name: "win" },
+    { name: "nft" },
+    { name: "···" },
+  ];
 
   return (
     <>
-    <h1  className = "tiffanyBrand">The moon is made of pancakes.</h1>
+      {/* <h1  className = "tiffanyBrand">The moon is made of pancakes.</h1>
     <h1  color = "pinkBrand">The moon is made of pancakes.</h1>
     <h1  className = "violetBrand">The moon is made of pancakes.</h1>
-    <Navbar menu = {menu}/>
-    <div>
-    <Tabs/>
-    </div>
-      <Routes>
-        <Route path='/' element={<Button color="violetBrand" onClick={()=>Navigate("/2")} buttonStyle={"btn--default"}>ciao</Button>} />
-        <Route path = "/1" element = {<Button buttonStyle={"btn--default"}/>}/>
-        <Route path="/2" element= {<Button  buttonSize={"btn--full"} type="button" as={Link} to="/">Esempio</Button>}/>
-      </Routes>
+    <Navbar menu = {menu}/> */}
+      {/* <div>
+        <Tabs />
+      </div> */}
+      <div>
+        <Paragraph />
+      </div>
+      {/* <Routes>
+        <Route
+          path="/"
+          element={
+            <Button
+              color="violetBrand"
+              onClick={() => Navigate("/2")}
+              buttonStyle={"btn--default"}
+            >
+              ciao
+            </Button>
+          }
+        />
+        <Route path="/1" element={<Button buttonStyle={"btn--default"} />} />
+        <Route
+          path="/2"
+          element={
+            <Button buttonSize={"btn--full"} type="button" as={Link} to="/">
+              Esempio
+            </Button>
+          }
+        />
+      </Routes> */}
     </>
   );
 }
 
 export default App;
-
-
-
