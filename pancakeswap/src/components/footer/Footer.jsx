38e567895650b6c6c8ce73b2f8cd8052f2761ce7
Elio Scaletta <elioscal@hotmail.it>
2022-09-20 16:41:38 +0200

    Creazione liste e parziale stilizzazione del footer.

--- pancakeswap/src/components/footer/Footer.jsx
+++ pancakeswap/src/components/footer/Footer.jsx
@@ -2,10 +2,32 @@ import React from "react";
 import { Button } from "../shared/button/Button";
 import "./_footer.scss";
 
+const about = ["Contact", "Brand", "Blog", "Community", "Litepaper", "--", "Online store"]
+const help = ["Customer Support", "Troubleshooting", "Guides"]
+const developers = ["Github", "Documentation", "Bug Bounty", "Audits", "Careers"]
+
+
 export function Footer(props) {
     return (
-        <div className="test">
-            <p>Questa è una prova</p>
+        <div className="footer">
+            <div className="footer-lists-container">
+                <ul>
+                    <span>ABOUT</span>
+                    {about.map((item) => <li key={item}>{item}</li>)}
+
+                </ul>
+                <ul>
+                    <span>HELP</span>
+                    {help.map((item) => <li key={item}>{item}</li>)}
+
+                </ul>
+                <ul>
+                    <span>DEVELOPERS</span>
+                    {developers.map((item) => <li key={item}>{item}</li>)}
+
+                </ul>
+                <img style={{width:"100px", height: "100px"}} src="./img1.png" alt="img"></img>
+            </div>
         </div>
     )
 }
