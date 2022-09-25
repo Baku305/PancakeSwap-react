863b4a096f11511327ceb460e82fb5c6146f238b
Baku305 <101564468+Baku305@users.noreply.github.com>
2022-09-25 10:51:07 +0200

    modal finished (no responsive)

--- pancakeswap/src/components/shared/setting-modal/settingModel.jsx
+++ pancakeswap/src/components/shared/setting-modal/settingModel.jsx
@@ -1,10 +1,10 @@
-import React, { useState, Fragment, useContext } from "react";
-import { Dialog, Transition } from "@headlessui/react";
+import React, { useState, useContext } from "react";
 import "../../../style.scss";
 import { Checkbox } from "../form/checkbox/Checkbox";
 import { Button } from "../button/Button";
 import { ThemeContext } from "../../../App";
-
+import closeIcon from "../../../assets/asset 38.svg"
+import infoIcon from "../../../assets/infoIcon.svg"
 export const settingArray = [
   {
     title: "global",
@@ -14,57 +14,93 @@ export const settingArray = [
         selector: "checkbox",
         selectorType: "theme",
         selectorNumber: 1,
+        
       },
       {
         title: "Subgraph Health Indicator",
         selector: "checkbox",
         selectorType: "normal",
         selectorNumber: 1,
+        info: "Turn on NFT market subgraph health indicator all the time. Default is to show the indicator only when the network is delayed",
       },
       {
         title: "Default Transaction Speed (GWEI)",
         selector: "button",
         selectorType: "btn--connect",
-        selectorNumber: ["standard(5)", "fast(6)", "instant(7)"],
+        selectorNumber: ["standard (5)", "fast (6)", "instant (7)"],
+        info: "Adjusts the gas price (transaction fee) for your transaction. Higher GWEI = higher speed = higher fees"
       },
     ],
   },
 ];
 
-
-
 export function MyDialog({ isOpen, setIsOpen, array }) {
-  
-  const {theme} = useContext(ThemeContext)
-  
+  const { theme } = useContext(ThemeContext);
+  const [btnSelected, setBtnSelected] = useState();
+
   return (
     <>
       {isOpen && (
         <div>
           <div className="wrapper" onClick={() => setIsOpen(false)}></div>
-          <div isOpen={isOpen} className = {`settingModal ${theme}-theme`}>
-            <h2 className="settingModalTitle">Settings</h2>
-            {array.map((el) => (
-              <div className="settingModalList">
-                <h3 className="settingModalListTitle">{el.title}</h3>
-                {el.content.map((content) => (
+          <div isOpen={isOpen} className={`settingModal ${theme}-theme`}>
+            <div className={`wrapperTitle ${theme}-theme`}>
+              <h3 className="settingModalTitle">Settings</h3>
+              <button className="wrapperTitleButton" onClick={()=>setIsOpen(false)}>
+                <svg
+                data-src={closeIcon}
+                />
+              </button>
+            </div>
+            {array.map((el, i) => (
+              <div key={i} className={`settingModalList ${theme}-theme`}>
+                <h3 className={`settingModalListTitle ${theme}-theme`}>{el.title}</h3>
+                {el.content.map((content, i) => (
                   <>
                     {content.selector === "checkbox" ? (
-                      <div className="settingModalListContent">
-                        <h4>{content.title}</h4>
+                      <div key={i} className={`settingModalListContent ${theme}-theme`}>
+                        <div className="settingModalListContentTitle">
+                          <h4>{content.title}</h4>
+                          {content.info && (
+                            <div className="settingModalListContentTitleInfo">
+                              <svg
+                              data-src={infoIcon}
+                              className={`${theme}-theme`}
+                              />
+                              <div className="settingModalListContentTitleInfoText"><h4>{content.info}</h4></div>
+                            </div>
+                          )}
+                        </div>
                         <Checkbox types={content.selectorType} />
                       </div>
                     ) : (
-                      <div className="settingModalListContentButton">
-                        <h4>{content.title}</h4>
-                        <div className = "buttonWrapper">
-                        {content.selectorNumber.map((sn) => (
-                            <Button
-                              buttonStyle={content.selectorType}
-                              children={<span>{sn}</span>}
-                            />
-                        ))}
-                          </div>
+                      <div key={i} className={`settingModalListContentButton ${theme}-theme`}>
+                        <div className="settingModalListContentButtonTitle">
+                          <h4>{content.title}</h4>
+                          {content.info && (
+                            <div className="settingModalListContentTitleInfo">
+                              <svg
+                              data-src={infoIcon}
+                              className={`${theme}-theme`}
+                              />
+                              <div className={`settingModalListContentTitleInfoText ${theme}-theme`}><h4>{content.info}</h4></div>
+                            </div>
+                          )}
+                        </div>
+                        <div className={`buttonWrapper ${theme}-theme`}>
+                          {content.selectorNumber.map((sn, i) => (
+                            <div
+                              onClick={() => setBtnSelected(i)}
+                              className={btnSelected === i && "btnSelected"}
+                            >
+                              <Button
+                                key={i}
+                                buttonStyle={content.selectorType}
+                                children={<span>{sn}</span>}
+                              />
+                            </div>
+                          ))}
+                        </div>
                       </div>
                     )}
                   </>
