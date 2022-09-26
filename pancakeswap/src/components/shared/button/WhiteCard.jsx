7f9d26999c4f6ec5be13b89c6e359095cd8175ff
Karim Hajouan <101986559+karimhaj@users.noreply.github.com>
2022-09-26 18:50:50 +0200

    fix cards positioning and responsive

--- pancakeswap/src/components/shared/button/WhiteCard.jsx
+++ pancakeswap/src/components/shared/button/WhiteCard.jsx
@@ -4,11 +4,13 @@ export const WhiteCard = ({icon, title, title2, title2Color , text}) =>{
         <div className="card-shadow">
         <div className="card-container">
         <div className="card-header">
-            {icon}
-        </div>
-        <div className="card-body">
+            <div className="card-header-icon">{icon}</div>
+            <div className="card-header-text">
             {title && <h2>{title}</h2>}
             {title2 && <h2 style={{color: title2Color}} className="card-space-body-footer">{title2}</h2>}
+            </div>
+        </div>
+        <div className="card-body">
             {text && <p>{text}</p>} 
         </div>
         </div>
